e8069a658f6692794fcaf150800b3a1af191e848
Keyang Li <liky1995@hotmail.com>
2026-02-01 10:39:40 +0800

    dev better ui

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export const content = [
+    "./component/**/*.{vue,js,ts,jsx,tsx}"
+];
+export const prefix = 'vpmc';
+export const theme = {
+    extend: {},
+};
+export const corePlugins = {
+    preflight: false,
+};
